424bbe6679e6052c3d7e54a59b70fdc10cf27481
TanishqR <102349259+tanishqr32@users.noreply.github.com>
2022-09-01 13:40:44 +0530

    doubt
    why is it not necessary to check where root->children[0] == NULL or not.
    if that is NULL then how will root->children->data work??

--- Trees/node with max child sum.cpp
+++ Trees/node with max child sum.cpp
@@ -9,15 +9,34 @@ class TreeNode {
    public:
     T data;
     vector<TreeNode<T>*> children;
-    /* we dont need to specify the template typename of treeNode, 
-       it will assume the 
-       typename same as the parent.*/
-
     TreeNode(T data) { this->data = data; }
-
     ~TreeNode() {
         for (int i = 0; i < children.size(); i++) {
             delete children[i];
         }
     }
 };
+
+TreeNode<int>* maxSumNode(TreeNode<int>* root){
+    if(root == NULL){
+        return NULL;
+    }
+    int max = root->data;
+    TreeNode<int>* ans = root;
+    for(int i = 0;i<root->children.size();i++){
+        max += root->children[i]->data;
+        TreeNode<int>* temp = maxSumNode(root->children[i]);
+        int t = temp->data;
+        for(int i = 0;i<temp->children.size();i++){
+            t += temp->children[i]->data;
+        }
+        if(t > max){
+            ans = temp;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    
+}
